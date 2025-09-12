b98b23c7385f22c00328c3449d7d708fbf7657af
rar34 <ra.cse34@gmail.com>
2025-09-12 18:46:57 +0600

    show category button into the webpage

--- script/video.js
+++ script/video.js
@@ -1,11 +1,19 @@
 const loadCategories = async() =>{
 const res = await fetch('https://openapi.programming-hero.com/api/phero-tube/categories');
 const data = await res.json();
-displayCatogories(data.categories)
+displayCategories(data.categories)
 }
 
-const displayCatogories = (data) =>{
-    console.log(data);
+const displayCategories = (data) =>{
+    data.map(item =>{
+        const categoriesContainer = document.getElementById('category-container');
+
+        const button = document.createElement('button');
+        button.classList = 'btn';
+        button.innerText = item.category;
+
+        categoriesContainer.append(button)
+    })
 }
 
 loadCategories();
